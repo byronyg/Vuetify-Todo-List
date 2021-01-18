7f4037bab7befbf0f50e96d6390e13a3b4444ad6
Byron Yego <byronyego@gmail.com>
2021-01-18 13:49:32 +0300

    master

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about pa-10">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque explicabo odio laudantium obcaecati laboriosam, ratione at quasi sit consequatur est! Neque ut saepe repudiandae quisquam a expedita earum eligendi unde.</p>
+   
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque explicabo odio laudantium obcaecati laboriosam, ratione at quasi sit consequatur est! Neque ut saepe repudiandae quisquam a expedita earum eligendi unde.</p>
+   
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque explicabo odio laudantium obcaecati laboriosam, ratione at quasi sit consequatur est! Neque ut saepe repudiandae quisquam a expedita earum eligendi unde.</p>
+    
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque explicabo odio laudantium obcaecati laboriosam, ratione at quasi sit consequatur est! Neque ut saepe repudiandae quisquam a expedita earum eligendi unde.</p>
+    
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque explicabo odio laudantium obcaecati laboriosam, ratione at quasi sit consequatur est! Neque ut saepe repudiandae quisquam a expedita earum eligendi unde.</p> 
+  </div>
+</template>
